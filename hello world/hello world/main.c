bee91c4bdbed8c7fc0e739eefe30949a6466af65
mikiwu121002 <152714949+mikiwu121002@users.noreply.github.com>
2024-09-07 17:53:20 +0200

    Add files via upload

---
+++ hello world/hello world/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hello world
+//
+//  Created by wu guang chen on 07/09/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("miki is better than kimi\n");
+    return 0;
+}
