b0256e64322f25b40d3c7689b32b0bc248281580
mikiwu121002 <152714949+mikiwu121002@users.noreply.github.com>
2024-12-22 15:00:57 +0100

    Add files via upload

---
+++ Printing the value of a variable/Printing the value of a variable/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Printing the value of a variable
+//
+//  Created by wu guang chen on 2024. 12. 22..
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    n= 1111111;
+    printf("the value is %d \n",n);
+    n=999999999;
+    printf("the value is %d \n",n);
+    return 0;
+}
